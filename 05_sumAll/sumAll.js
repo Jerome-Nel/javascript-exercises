78f1a8e922b9430ff21fb5a1a88dd845907d7389
Jerome-Nel <populi.nel@gmail.com>
2022-06-07 08:14:16 +0800

    Add solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    // check if value is a valid integer
+    if (!Number.isInteger(start) || !Number.isInteger(end)) return "ERROR";
+    // check if value is negative
+    if (start < 0 || end < 0) return "ERROR";
+    // swap value if start is greater than end
+    if (start > end) {
+        let temp = start;
+        start = end;
+        end = temp;
+    }
+    let sum = 0;
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
